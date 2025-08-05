ccdb1341b91685dcb1551965564d7bc87f8189e5
xcursocr <xcursocr@gmail.com>
2025-08-05 16:52:36 -0600

    Creando hero de categorias y productos

--- components/main/home/index.jsx
+++ components/main/home/index.jsx
@@ -1,11 +1,17 @@
 import { allProducts } from "@/data/products";
 import { Carousell } from "./Carousell";
+import { CardCategory } from "./CardCategory";
 
 export function IndexHome() {
   return (
     <section className="px-3 md:px-10">
       <div className="">
         <Carousell products={allProducts} />
+      </div>
+      <div className="flex sm:flex-row flex-col gap-6 px-6 max-w-3xl">
+        <CardCategory />
+        <CardCategory />
+        <CardCategory />
       </div>
       <div>
         <h1>Home page</h1>
